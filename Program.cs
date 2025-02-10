b36f26008a4c223db128c52484b5e048521ed780
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2025-02-10 16:13:33 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Register services to the container.
+builder.Services.AddHttpClient<GitHubService>(); // Add your GitHub service
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration); // Add IConfiguration to DI container
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
