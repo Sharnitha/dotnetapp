59b4a5e0196fc144b573110a706316c42222177c
Ubuntu <Sharnitha@testvirtualmachine.xhhnigjunhtefglsbtw4f43luc.bx.internal.cloudapp.net>
2025-02-10 10:06:34 +0000

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnettestapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
